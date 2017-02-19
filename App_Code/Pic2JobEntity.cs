a2a945259464f2ca48096fd6643a608bcd074ab5
PIC2JOB <pic2job@outlook.es>
2017-02-19 22:04:05 +0100

    Add files via upload

---
+++ App_Code/Pic2JobEntity.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Pic2Job.Entity
+{
+    public static partial class Pic2JobEntity
+    {
+        static pic2jobEntities contexto = new pic2jobEntities();
+
+        //Funcion que comprueba si el nick esta repetido.
+        public static bool comprobarNick(string nick)
+        {
+            bool retorno = false;
+
+            usuarios usuari = 
+                (from c in contexto.usuarios
+                 where c.nick == nick
+                 select c).FirstOrDefault();
+
+            if (usuari != null)
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        //Funcion que comprueba si el correo esta repetido.
+        public static bool comprobarEmail(string email)
+        {
+            bool retorno = false;
+
+            usuarios usuari =
+                (from c in contexto.usuarios
+                 where c.correo == email
+                 select c).FirstOrDefault();
+
+
+            if (usuari != null)
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        //Funcion que comprueba si el telefono esta repetido.
+        public static bool comprobarTelefono(string telefono)
+        {
+            bool retorno = false;
+
+            usuarios usuari =
+                (from c in contexto.usuarios
+                 where c.telefono == telefono
+                 select c).FirstOrDefault();
+
+
+            if (usuari != null)
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        //Funcion comprueba hace el login.
+        public static string comprobarUsuariPass(string email, string pass)
+        {
+            string retorno;
+
+            usuarios usuari = 
+                (from c in contexto.usuarios
+                 where c.correo == email && c.pass == pass 
+                 select c).FirstOrDefault();
+
+            if (usuari != null)
+            {
+                retorno = usuari.id_usuario.ToString();
+            }
+            else
+            {
+                retorno = "Usuario o contraseña incorrectos!";
+            }
+            return retorno;
+        }
+
+        //Funcion que guarda un usuario.
+        public static String InsertarUsuario(String nick, String nombre, String apellidos, String email, String telefono, String pass)
+        {
+            usuarios usuari = new usuarios();
+
+            usuari.nick = nick;
+            usuari.nombre = nombre;
+            usuari.apellidos = apellidos;
+            usuari.correo = email;
+            usuari.telefono = telefono;
+            usuari.pass = pass;
+            usuari.id_ciudad = 1;
+
+            contexto.usuarios.Add(usuari);
+
+            try
+            {
+                contexto.SaveChanges();
+
+                return "Usuario Registrado!";
+            }
+            catch (DbEntityValidationException ex)
+            {
+                return ex.EntityValidationErrors.ToString();
+            }
+        }
+
+        //Funcion que obtiene todas las imagenes por id de usuario, retorna una lista de imagenes.
+        public static List<imagenes> FindImatgesById(int id)
+        {
+            List<imagenes> imatges =
+                (from c in contexto.imagenes
+                 where c.id_usuario == id
+                 select c).ToList();
+
+            return imatges;
+        }
+
+        //funcion que devuelve el usuario con el id indicado
+        public static usuarios FindUsuariById(int id)
+        {
+            usuarios usuari =
+                (from c in contexto.usuarios
+                 where c.id_usuario == id
+                 select c).FirstOrDefault();
+
+            return usuari;
+        }
+
+        //Funcion que modifica un usuario
+        public static void modificarUsuario(int id, string campo_modificado, string valor)
+        {
+            usuarios usuari = FindUsuariById(id);
+
+            switch (campo_modificado)
+            {
+                case ("nick"):
+                    usuari.nick = valor;
+                    break;
+                case ("nombre"):
+                    usuari.nombre = valor;
+                    break;
+                case ("apellido"):
+                    usuari.apellidos = valor;
+                    break;
+                case ("correo"):
+                    usuari.correo = valor;
+                    break;
+                case ("telefono"):
+                    usuari.telefono = valor;
+                    break;
+                case ("sexo"):
+                    usuari.sexo = bool.Parse(valor);
+                    break;
+                case ("peso"):
+                    usuari.peso = double.Parse(valor);
+                    break;
+                case ("altura"):
+                    usuari.altura = double.Parse(valor);
+                    break;
+                case ("grupoetnico"):
+                    usuari.grupo_etnico = int.Parse(valor);
+                    break;
+                case ("fecha"):
+                    int año = int.Parse(valor.Substring(6, 4)), 
+                        mes = int.Parse(valor.Substring(3, 2)), 
+                        dia = int.Parse(valor.Substring(0, 2));
+                    DateTime fecha = new DateTime(año,mes,dia);
+                    usuari.fecha_nacimiento = fecha;
+                    break;
+                case ("ciudad"):
+                    break;
+                case ("trabajo"):
+                    usuari.tipo_trabajo = int.Parse(valor);
+                    break;
+            }
+            contexto.SaveChanges();
+        }
+
+        //funcion que busca una imagen por id de imagen
+        public static imagenes FindImatgeById(int id)
+        {
+            imagenes imatge =
+                (from c in contexto.imagenes
+                 where c.id == id
+                 select c).FirstOrDefault();
+
+            return imatge;
+        }
+
+        //funcion que añade un registro de una imagen subida ala base de datos
+        public static void InsertImagen(String source, int id)
+        {
+            imagenes imagen = new imagenes();
+
+            imagen.src = source;
+            imagen.id_usuario = id;
+
+            contexto.imagenes.Add(imagen);
+
+            contexto.SaveChanges();
+        }
+
+        //funcion que borra una imagen con un id en concreto.
+        public static String DeleteImatge(int id)
+        {
+            String missatge = "";
+
+            imagenes imatge = FindImatgeById(id);
+
+            try
+            {
+                contexto.imagenes.Remove(imatge);
+                contexto.SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                SqlException sqlEx = (SqlException)ex.InnerException.InnerException;
+                missatge = sqlEx.Message;
+
+                contexto.imagenes.Add(imatge);
+            }
+
+            return missatge;
+        }
+    }
+}
+
+
+
+
---
+++ App_Code/UploadImagen.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Web.UI.WebControls;
+using System.Runtime.InteropServices;
+
+namespace Pic2Job.UploadImagen
+{
+    public static partial class UploadImagen
+    {
+        public static String SubirImagen(FileUpload subirImagen, int id)
+        {
+            //comprueba si hay archivo
+            if (subirImagen.HasFile)
+            {
+                //comprueba si es una imagen
+                if (subirImagen.PostedFile.ContentType == "image/jpeg" || subirImagen.PostedFile.ContentType == "image/gif" || subirImagen.PostedFile.ContentType == "image/png")
+                {
+                    //comprueba si pesa menos de 15mB.
+                    if (subirImagen.PostedFile.ContentLength < 15985760)
+                    {
+                        //crea el nombre de la imagen con la fecha del momento + el id + la extension.
+                        DateTime fecha = DateTime.Now;
+                        string ext = Path.GetExtension(subirImagen.FileName);
+                        string nombreImagen = id + "_" + fecha.Day.ToString() + fecha.Month.ToString() + fecha.Year.ToString() + "_" + fecha.Hour.ToString() + fecha.Minute.ToString() + fecha.Second.ToString() + fecha.Millisecond.ToString() + ext;
+
+                        //comprueba si existe el directorio en el servidor, sino lo crea.
+                        if (!Directory.Exists(System.Web.HttpContext.Current.Server.MapPath(@"IMAGENES_SUBIDAS\" + id))) ;
+                        {
+                            Directory.CreateDirectory(System.Web.HttpContext.Current.Server.MapPath(@"IMAGENES_SUBIDAS\" + id));
+                        }
+
+                        //guardamos la ruta de la carpeta de imagenes del usuario.
+                        string carpeta = System.Web.HttpContext.Current.Server.MapPath(@"IMAGENES_SUBIDAS\" + id + "\\");
+
+                        //creamos un bitmap con la imagen subida.
+                        Bitmap BMPoriginal = new Bitmap(subirImagen.FileContent);
+
+                        //variables para redimensionar la imagen.
+                        int newHeight = 0, newWidth = 0;
+                        Bitmap BMP;
+
+                        //si es una imagen horizontal:
+                        //si la imagen es mas ancha que 1600 calcula la proporcion de la imagen y 
+                        //la nueva altura, y la guarda en el bitmap definitivo con las medidas 
+                        //establecidas para que sea maximo 1600 de ancho, si es menos 
+                        //ancha que 1600 la guardamos tal cual.
+                        if (BMPoriginal.Width >= BMPoriginal.Height)
+                        {
+                            if (BMPoriginal.Width > 1600)
+                            {
+                                decimal origWidth = BMPoriginal.Width;
+                                decimal origHeight = BMPoriginal.Height;
+                                decimal imgRatio = origHeight / origWidth;
+                                newWidth = 1600; //ANCHO PREDEFINIDO
+                                decimal newHeight_temp = newWidth * imgRatio;
+                                newHeight = Convert.ToInt16(newHeight_temp);
+
+                                BMP = new Bitmap(BMPoriginal, newWidth, newHeight);
+                            }
+                            else
+                            {
+                                BMP = BMPoriginal;
+                            }
+                        }
+                        //else si es una imagen vertical:
+                        //si la imagen es mas alta que 1600 calcula la proporcion de la imagen y 
+                        //el nuevo ancho, y la guarda en el bitmap definitivo con las medidas 
+                        //establecidas para que sea maximo 1600 de alto, si es menos 
+                        //alta que 1600 la guardamos tal cual.
+                        else
+                        {
+                            if (BMPoriginal.Height > 1600)
+                            {
+                                decimal origWidth = BMPoriginal.Width;
+                                decimal origHeight = BMPoriginal.Height;
+                                decimal imgRatio = origWidth / origHeight;
+                                newHeight = 1600; //Alto PREDEFINIDO
+                                decimal newWidth_temp = newHeight * imgRatio;
+                                newWidth = Convert.ToInt16(newWidth_temp);
+
+                                BMP = new Bitmap(BMPoriginal, newWidth, newHeight);
+                            }
+                            else
+                            {
+                                BMP = BMPoriginal;
+                            }
+                        }
+
+                        //convertimos el bitmap en un grafico y le aplicamos unos retoques de calidad.
+                        Graphics Grafico = Graphics.FromImage(BMP);
+                        Grafico.SmoothingMode = SmoothingMode.AntiAlias;
+                        Grafico.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                        Grafico.DrawImage(BMPoriginal, 0, 0, newWidth, newHeight);
+
+                        //probamos de guardar la imagen en el servidor, y capturamos excepcion.
+                        try
+                        {
+                            BMP.Save(carpeta + nombreImagen);
+                        }
+                        catch (ExternalException ex)
+                        {
+                            return ex.InnerException.Message;
+                        }
+
+                        //vaciamos los objetos usados.
+                        BMPoriginal.Dispose();
+                        BMP.Dispose();
+                        Grafico.Dispose();
+
+                        //retornamos la ruta exacta de la imagen para poder guardala en la base de datos.
+                        return "IMAGENES_SUBIDAS\\" + id + "\\" + nombreImagen;
+                    }
+                    //distintos mensajes de error de los ifs del principio de la funcion.
+                    else
+                    {
+                        return "ERROR: Tamaño maximo 15MB.";
+                    }
+                }
+                else
+                {
+                    return "ERROR: El archivo seleccionado no es una imagen.";
+                }
+            }
+            else
+            {
+                return "ERROR: Selecciona una imagen porfavor";
+            }
+        }
+    }
+}
